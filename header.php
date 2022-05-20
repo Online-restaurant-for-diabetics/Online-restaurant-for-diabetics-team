2549c8f40aa4b5a66915c57e05f32686b524d0d7
elham hejo <elhamhejo0@gmail.com>
2022-05-20 23:11:12 +0300

    add-style-header-and-js-and-configrationForOurProject #34

---
+++ header.php
@@ -0,0 +1,27 @@
+
+<header class="header">
+<link rel="stylesheet" href="css/style.css">
+
+   <div class="flex">
+
+      <a href="#" class="logo">BASH RESTURANT</a>
+
+      <nav class="navbar">
+         <a href="add.php">add products</a>
+         <a href="products.php">view products</a>
+      </nav>
+
+      <?php
+      
+      $select_rows = mysqli_query($conn, "SELECT * FROM `cart`") or die('query failed');
+      $row_count = mysqli_num_rows($select_rows);
+
+      ?>
+
+      <a href="cart.php" class="cart">cart <span><?php echo $row_count; ?></span> </a>
+
+      <div id="menu-btn" class="fas fa-bars"></div>
+
+   </div>
+
+</header>
