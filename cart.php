e58f2156c5b3400eadf9ccb564d8aed7a3d59e6e
Shatha-AbuShammala <shadamhr@gmail.com>
2022-05-20 19:07:54 +0300

    creat deletall,grand total ,continue shopping,check out relat #16

---
+++ cart.php
@@ -0,0 +1,59 @@
+<?php
+
+@include 'config.php';
+ 
+if(isset($_GET['remove'])){
+   $remove_id = $_GET['remove'];
+   mysqli_query($conn, "DELETE FROM `cart` WHERE id = '$remove_id'");
+   header('location:cart.php');
+};
+
+if(isset($_GET['delete_all'])){
+   mysqli_query($conn, "DELETE FROM `cart`");
+   header('location:cart.php');
+}
+
+?>
+
+<body>
+
+   <table>
+      <tbody>
+
+         <?php 
+         
+         $select_cart = mysqli_query($conn, "SELECT * FROM `cart`");
+         $grand_total = 0;
+         if(mysqli_num_rows($select_cart) > 0){
+            while($fetch_cart = mysqli_fetch_assoc($select_cart)){
+         ?>
+         
+         <?php
+           $grand_total += $sub_total;  
+            };
+         };
+         ?> 
+         <tr class="table-bottom">
+            <td><a href="products.php" class="option-btn" style="margin-top: 0;">continue shopping</a></td>
+            <td colspan="3">grand total</td>
+            <td>$<?php echo $grand_total; ?>/-</td>
+            <td><a href="cart.php?delete_all" onclick="return confirm('are you sure you want to delete all?');" class="delete-btn"> <i class="fas fa-trash"></i> delete all </a></td>
+         </tr>
+
+      </tbody>
+
+   </table>
+
+   <div class="checkout-btn">
+      <a href="checkout.php" class="btn <?= ($grand_total > 1)?'':'disabled'; ?>">procced to checkout</a>
+   </div>
+
+</section>
+
+</div>
+   
+<!-- custom js file link  -->
+<script src="js/script.js"></script>
+
+</body>
+</html>
