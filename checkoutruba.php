dd3ce4d7f536de01183eed7a87a6b0a3e07cf42f
rubajendeya <102482446+rubajendeya@users.noreply.github.com>
2022-05-21 09:07:07 +0300

    Add files via upload

---
+++ checkoutruba.php
@@ -0,0 +1,74 @@
+<?php
+
+@include 'config.php';
+
+if(isset($_POST['order_btn'])){
+
+   $name = $_POST['name'];
+   $number = $_POST['number'];
+   $email = $_POST['email'];
+   $method = $_POST['method'];
+   $flat = $_POST['flat'];
+   $street = $_POST['street'];
+   $city = $_POST['city'];
+   $state = $_POST['state'];
+   $country = $_POST['country'];
+   $pin_code = $_POST['pin_code'];
+
+   $cart_query = mysqli_query($conn, "SELECT * FROM `cart`");
+   $price_total = 0;
+   if(mysqli_num_rows($cart_query) > 0){
+      while($product_item = mysqli_fetch_assoc($cart_query)){
+         $product_name[] = $product_item['name'] .' ('. $product_item['quantity'] .') ';
+         $product_price = number_format($product_item['price'] * $product_item['quantity']);
+         $price_total += $product_price;
+      };
+   };
+
+   $total_product = implode(', ',$product_name);
+   $detail_query = mysqli_query($conn, "INSERT INTO `order`(name, number, email, method, flat, street, city, state, country, pin_code, total_products, total_price) VALUES('$name','$number','$email','$method','$flat','$street','$city','$state','$country','$pin_code','$total_product','$price_total')") or die('query failed');
+
+   if($cart_query && $detail_query){
+      echo "
+      <div class='order-message-container'>
+      <div class='message-container'>
+         <h3>thank you for shopping!</h3>
+         <div class='order-detail'>
+            <span>".$total_product."</span>
+            <span class='total'> total : $".$price_total."/-  </span>
+         </div>
+         <div class='customer-details'>
+            <p> your name : <span>".$name."</span> </p>
+            <p> your number : <span>".$number."</span> </p>
+            <p> your email : <span>".$email."</span> </p>
+            <p> your address : <span>".$flat.", ".$street.", ".$city.", ".$state.", ".$country." - ".$pin_code."</span> </p>
+            <p> your payment mode : <span>".$method."</span> </p>
+            <p>(*pay when product arrives*)</p>
+         </div>
+            <a href='products.php' class='btn'>continue shopping</a>
+         </div>
+      </div>
+      ";
+   }
+
+}
+
+?>
+
+<!DOCTYPE html>
+<html lang="en">
+<head>
+   <meta charset="UTF-8">
+   <meta http-equiv="X-UA-Compatible" content="IE=edge">
+   <meta name="viewport" content="width=device-width, initial-scale=1.0">
+   <title>checkout</title>
+
+   <!-- font awesome cdn link  -->
+   <link rel="stylesheet" href="https://cdnjs.cloudflare.com/ajax/libs/font-awesome/6.0.0/css/all.min.css">
+
+   <!-- custom css file link  -->
+   <link rel="stylesheet" href="css/style.css">
+
+</head>
+<body>
+
